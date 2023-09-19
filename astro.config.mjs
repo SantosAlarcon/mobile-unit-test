b02e0556aecbd14c7275c14a96a42baaa44b5f34
Santos Alarcón <santosalarcon86@gmail.com>
2023-09-19 19:59:06 +0200

    Added footer component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://santos-alarcon.github.io",
-    base: "/mobile-unit-test"
+    //base: "/mobile-unit-test"
 });
